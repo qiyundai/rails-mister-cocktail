e6498ca1cb0ff3b6b532b302d393980d4b578c19
Qiyun Dai <pianoquin@gmail.com>
2021-02-20 14:20:55 -0600

    backend done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -15,6 +15,12 @@ class DosesController < ApplicationController
         end
     end
 
+    def destroy
+        @dose = Dose.find(params[:id])
+        @dose.destroy
+        redirect_to cocktail_path(@dose.cocktail)
+    end
+
     def strong_params
         params.require(:dose).permit(:description, :ingredient_id)
     end
